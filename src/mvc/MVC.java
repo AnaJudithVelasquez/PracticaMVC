8716939704cdcb03a204893e233b8f11b6cdbaea
Ana Velasquez <anajudith1309@gmail.com>
2025-04-22 19:59:56 -0500

    Se completa la logica de las clases

--- src/mvc/MVC.java
+++ src/mvc/MVC.java
@@ -1,12 +1,27 @@
 
 package mvc;
 
+import Modelo.Estudiante; 
+import Vista.VistaEstudiante;
+import Controlador.ControladorEstudiante;
 
 public class MVC {
 
     
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //Inicializamos el modelo creando un nuevo estudiante
+        Estudiante estudiante1 = new Estudiante();
+        estudiante1.setNombre("Pepito");
+        estudiante1.setEdad(17);
+        
+        //Inicializamos 
+        VistaEstudiante vista = new VistaEstudiante();
+        ControladorEstudiante controlador = new ControladorEstudiante(estudiante1, vista );
+        
+        controlador.actualizarVista();
+        
     }
     
 }
